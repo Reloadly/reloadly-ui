9e028651f4c6b9161c2d7dd253025ba44a996234
Osama Gamal <ogamalii65@gmail.com>
2023-05-25 15:12:47 +0300

    extend action card style

--- src/lib/containers/action-card/action-card.component.ts
+++ src/lib/containers/action-card/action-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { ActionCardStyle } from 'dist/reloadly-ui/lib/enums/card-style';
 
 @Component({
     selector: 'app-action-card',
@@ -11,14 +12,12 @@ export class ActionCardComponent {
     @Input() actionText: string = '';
     @Input() cardWidth: string = '302px';
     @Input() cardHeight: string = '153px';
+    @Input() toggleValue = false;
     @Input() disableAction: boolean = true;
+    @Input() cardStyle: ActionCardStyle = ActionCardStyle.basic;
     @Input() toggleTitle: String | undefined = 'Notify me';
     @Output() onActionClicked: EventEmitter<void> = new EventEmitter();
     @Output() onToggleChange: EventEmitter<boolean> = new EventEmitter();
-
-
-    @Input() toggleValue = false;
-
 
     toggleChange() {
         this.toggleValue = !this.toggleValue;
