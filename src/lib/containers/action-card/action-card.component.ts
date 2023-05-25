5056f5c9e019233d459da28925cb3f98b5d9e1ec
Osama Gamal <ogamalii65@gmail.com>
2023-05-25 15:12:47 +0300

    extend action card style

--- src/lib/containers/action-card/action-card.component.ts
+++ src/lib/containers/action-card/action-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { ActionCardStyle } from 'dist/reloadly-ui/lib/enums/card-style';
 
 @Component({
     selector: 'app-action-card',
