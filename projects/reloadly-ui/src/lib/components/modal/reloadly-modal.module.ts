d0bc68a081c63c34ca6f3c786fd34bd30d8aceda
SabreFoxx <geraldnebs@gmail.com>
2023-07-20 13:56:22 +0100

    fix bugs on modal

--- projects/reloadly-ui/src/lib/components/modal/reloadly-modal.module.ts
+++ projects/reloadly-ui/src/lib/components/modal/reloadly-modal.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReloadlyModalContainerComponent } from './reloadly-modal-container/reloadly-modal-container.component';
+import { ReloadlyModalOutletComponent } from './reloadly-modal-outlet/reloadly-modal-outlet.component';
+import { ReloadlyModal } from './reloadly-modal';
 
 @NgModule({
-  declarations: [ReloadlyModalContainerComponent],
-  imports: [CommonModule]
+    declarations: [ReloadlyModalContainerComponent, ReloadlyModalOutletComponent],
+    imports: [CommonModule],
+    exports: [ReloadlyModalOutletComponent],
+    providers: [{ provide: ReloadlyModal, useExisting: ReloadlyModal }]
 })
 export class ReloadlyModalModule { }
