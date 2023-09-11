e0cd041e3f6d9fb679179af10f6d31506fbfe081
Johnny <johnnytmd5@gmail.com>
2023-09-11 11:40:26 +0300

    remove directive

--- src/lib/directives/directives.module.ts
+++ src/lib/directives/directives.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
-import { OutsideClickDirective } from './outside-click.directive';
 import { TableDirective } from './table.directive';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
     imports: [CommonModule],
     exports: [
-        OutsideClickDirective,
         TableDirective
     ],
     declarations: [
-        OutsideClickDirective,
         TableDirective
     ],
     providers: [],
