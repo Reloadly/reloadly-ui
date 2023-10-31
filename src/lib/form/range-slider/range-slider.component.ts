16a18b75144abc931fe8f7953c3fbf0a4e2df9bd
sabrefoxx <geraldnebs@gmail.com>
2023-10-31 17:54:14 +0100

    improvement and bugfixes to slider

--- src/lib/form/range-slider/range-slider.component.ts
+++ src/lib/form/range-slider/range-slider.component.ts
@@ -6,10 +6,12 @@ import {
     EventEmitter,
     Inject,
     Input,
+    OnChanges,
     OnDestroy,
     OnInit,
     Output,
     Renderer2,
+    SimpleChanges,
     ViewChild
 } from '@angular/core';
 import { ReplaySubject, Subject, takeUntil } from 'rxjs';
@@ -19,9 +21,9 @@ import { ReplaySubject, Subject, takeUntil } from 'rxjs';
     templateUrl: './range-slider.component.html',
     styleUrls: ['./range-slider.component.scss']
 })
-export class RangeSliderComponent implements OnInit, AfterViewInit, OnDestroy {
-    @Input() min!: number;
-    @Input() max!: number;
+export class RangeSliderComponent implements OnInit, AfterViewInit, OnChanges, OnDestroy {
+    @Input() set min(value: number) { this._min = Math.max(0, value) };
+    @Input() set max(value: number) { this._max = Math.max(this.min, value) };
     @Input() symbol: string = '';
     @Input() showInput: boolean = true;
     @Input() baseSize: number = 30;
@@ -36,12 +38,17 @@ export class RangeSliderComponent implements OnInit, AfterViewInit, OnDestroy {
     @ViewChild('knob') knob!: ElementRef;
     @ViewChild('input') input!: ElementRef;
 
+    public get min(): number { return this._min }
+    public get max(): number { return this._max }
+
     private knobListeners = new Array<() => void>;
     private windowListeners = new Array<() => void>;
     private window;
 
-    rawValue: number = 0;
-    private value = new Subject<[value: number, percentage: number]>();
+    private _min = 0;
+    private _max = 0;
+    private value: number = 0;
+    private values = new Subject<[value: number, percentage: number, emitEvent: boolean]>();
     private subs: ReplaySubject<boolean> = new ReplaySubject(1);
 
     constructor(private renderer: Renderer2, @Inject(DOCUMENT) private document: Document) {
@@ -49,10 +56,13 @@ export class RangeSliderComponent implements OnInit, AfterViewInit, OnDestroy {
     }
 
     ngOnInit(): void {
-        this.value.pipe(takeUntil(this.subs))
+        this.values.pipe(takeUntil(this.subs))
             .subscribe(currentValue => {
-                this.currentValueChange.emit(currentValue[0]);
-                this.currentPercentage.emit(currentValue[1]);
+                const emitEvent = currentValue[2];
+                if (emitEvent) {
+                    this.currentValueChange.emit(currentValue[0]);
+                    this.currentPercentage.emit(currentValue[1]);
+                }
             });
     }
 
@@ -65,12 +75,14 @@ export class RangeSliderComponent implements OnInit, AfterViewInit, OnDestroy {
 
         this.knobListeners = [
             this.renderer.listen(this.knob.nativeElement, 'mousedown', event => this.enableSlide(event)),
-            this.renderer.listen(this.knob.nativeElement, 'touchstart', event => this.enableSlide(event)),
+            // this.renderer.listen(this.knob.nativeElement, 'touchstart', event => this.enableSlide(event)),
         ];
     }
 
-    get formattedValue(): string {
-        return (this.symbol ? (this.symbol + ' ') : '') + (this.rawValue ?? '');
+    ngOnChanges(changes: SimpleChanges): void {
+        if ((changes?.['currentValue']?.['currentValue'] && this.max) || (changes?.['max']?.['currentValue'] && this.value)) {
+            this.setCurrentValues(changes['currentValue']['currentValue'], false);
+        }
     }
 
     ngOnDestroy(): void {
@@ -81,32 +93,51 @@ export class RangeSliderComponent implements OnInit, AfterViewInit, OnDestroy {
     }
 
     public inputChanged(event: Event): void {
-        this.setCurrentValue(parseInt((event.target as HTMLInputElement).value) || 0);
+        this.setCurrentValues(parseInt((event.target as HTMLInputElement).value) || 0);
+    }
+
+    get formattedValue(): string {
+        return (this.symbol ? (this.symbol + ' ') : '') + (+this.value ?? '');
+    }
+
+    get formattedMin(): string {
+        return (this.symbol ?? '') + (+this.min);
+    }
+
+    get formattedMax(): string {
+        return (this.symbol ?? '') + (+this.max);
     }
 
     private setCurrentValue(value: number): void {
+        this.value = value;
+    }
+
+    private setCurrentValues(value: number, emitEvent = true) {
         const difference = this.max - this.min;
-        const percentage = Math.round((value / difference) * 100);
-        this.value.next([value, percentage]);
+        let mappedValue = Math.max(value, this.min) - this.min;
+        const percentage = Math.round((mappedValue / difference) * 100);
+        this.values.next([value, percentage, emitEvent]);
+        this.value = value;
         this.moveSlider(percentage);
     }
 
-    private changeValue(percentage: number): void {
-        const value = Math.round((percentage / 100) * this.max);
-        this.value.next([value, percentage]);
+    private changeValue(percentage: number, emitEvent = true): void {
+        const difference = this.max - this.min;
+        const value = Math.round((percentage / 100) * difference) + this.min;
+        this.values.next([value, percentage, emitEvent]);
         this.renderer.setProperty(this.input.nativeElement, 'value', value);
-        this.rawValue = value;
+        this.value = value;
     }
 
     private enableSlide(event: Event): void {
         this.windowListeners.forEach(eventEnder => eventEnder());
         this.windowListeners = [
             this.renderer.listen(this.window, 'mouseup', event => this.disableSlide(event)),
-            this.renderer.listen(this.window, 'touchend', event => this.disableSlide(event)),
+            // this.renderer.listen(this.window, 'touchend', event => this.disableSlide(event)),
             // we want the user to be able to slide, even if his finger is outside the slider
             // and that's why we used window
             this.renderer.listen(this.window, 'mousemove', event => this.adjustSlider(event)),
-            this.renderer.listen(this.window, 'touchmove', event => this.adjustSlider(event))
+            // this.renderer.listen(this.window, 'touchmove', event => this.adjustSlider(event))
         ]
     }
 
@@ -123,8 +154,7 @@ export class RangeSliderComponent implements OnInit, AfterViewInit, OnDestroy {
                 xLength = event.touches[0].pageX - slider.getBoundingClientRect().left; // TODO test on touchscreen devices
             }
 
-            let percentage = this.clip(Math.round((xLength / slider.offsetWidth) * 100));
-
+            let percentage = this.clamp(Math.round((xLength / slider.offsetWidth) * 100));
             this.window?.requestAnimationFrame(() => {
                 this.moveSlider(percentage);
             });
@@ -132,18 +162,18 @@ export class RangeSliderComponent implements OnInit, AfterViewInit, OnDestroy {
         }
     }
 
-    private clip(number: number, min = 0, max = 100): number {
+    private clamp(number: number, min = 0, max = 100): number {
         if (number < min) return min;
         if (number > max) return max;
         return number;
     }
 
     private moveSlider(percentage: number): void {
-        percentage = this.clip(percentage);
-        const sliderWidth = this.range.nativeElement.offsetWidth - this.knob.nativeElement.offsetWidth;
+        percentage = this.clamp(percentage);
+        const sliderWidth = this.range?.nativeElement.offsetWidth - this.knob?.nativeElement.offsetWidth;
         let x = Math.round((percentage / 100) * sliderWidth);
-        x = this.clip(x, 0, sliderWidth);
-        this.knob.nativeElement.style.left = x + "px";
+        x = this.clamp(x, 0, sliderWidth);
+        if (this.knob.nativeElement) this.knob.nativeElement.style.left = x + "px";
     }
 
 }
