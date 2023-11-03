9ff15fd6e716f5a73f19c629419412eb85a09381
Johnny <johnnytmd5@gmail.com>
2023-11-03 11:56:36 +0200

    version bump

--- src/lib/components/accordion/accordion.module.ts
+++ src/lib/components/accordion/accordion.module.ts
@@ -3,27 +3,25 @@ import { ReloadlyAccordionComponent } from './accordion.component';
 import { ReloadlyAccordionItem } from "./directives/accordion-item.directive";
 import { ReloadlyAccordionItemContent } from "./directives/accordion-item-content.directive";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 
 
 
 @NgModule({
-  imports: [
-    CommonModule,
-    BrowserModule,
-    BrowserAnimationsModule],
-  exports: [
-    ReloadlyAccordionComponent,
-    ReloadlyAccordionItem,
-    ReloadlyAccordionItemContent,
+    imports: [
+        CommonModule,
+        BrowserAnimationsModule],
+    exports: [
+        ReloadlyAccordionComponent,
+        ReloadlyAccordionItem,
+        ReloadlyAccordionItemContent,
 
-  ],
-  declarations: [
-    ReloadlyAccordionComponent,
-    ReloadlyAccordionItem,
-    ReloadlyAccordionItemContent,
-  ],
-  providers: [],
+    ],
+    declarations: [
+        ReloadlyAccordionComponent,
+        ReloadlyAccordionItem,
+        ReloadlyAccordionItemContent,
+    ],
+    providers: [],
 })
 export class ReloadlyAccordionModule { }
