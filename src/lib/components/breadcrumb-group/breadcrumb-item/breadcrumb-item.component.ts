f9cb9225a05c8060de1d19456f3e6b080c97eafd
SabreFoxx-Reloadly <142517056+SabreFoxx-Reloadly@users.noreply.github.com>
2023-11-30 10:37:24 +0100

    Feature/bug fixes (#43)
    * add icon, bug fix
    
    * bug fixes
    
    * bump version
    
    ---------
    
    Co-authored-by: sabrefoxx <geraldnebs@gmail.com>

--- src/lib/components/breadcrumb-group/breadcrumb-item/breadcrumb-item.component.ts
+++ src/lib/components/breadcrumb-group/breadcrumb-item/breadcrumb-item.component.ts
@@ -1,10 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { AfterContentInit, ChangeDetectorRef, Component, Input, TemplateRef, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'reloadly-breadcrumb-item',
     templateUrl: './breadcrumb-item.component.html',
     styleUrls: ['./breadcrumb-item.component.scss']
 })
-export class BreadcrumbItemComponent {
+export class BreadcrumbItemComponent implements AfterContentInit {
     @Input() isActive: boolean = false;
+    @ViewChild(TemplateRef) template!: TemplateRef<any>;
+    index!: number;
+
+    constructor(private changeDetection: ChangeDetectorRef) { }
+
+    ngAfterContentInit(): void {
+        this.changeDetection.detectChanges();
+    }
 }
